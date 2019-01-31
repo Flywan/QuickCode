151f2373a2636049c7353ead8031fc5ead531322
Flywan <864274107@qq.com>
2019-01-31 17:25:36 +0800

    添加获得App应用签名信息

--- DeviceInfoUtil.java
+++ DeviceInfoUtil.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.hardware.display.DisplayManager;
 import android.os.Build;
 import android.os.Environment;
@@ -14,11 +15,12 @@ import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.WindowManager;
 
+import java.io.ByteArrayInputStream;
 import java.lang.reflect.MalformedParametersException;
+import java.security.MessageDigest;
+import java.security.cert.CertificateFactory;
+import java.security.cert.X509Certificate;
 
-/*
-* 工具类，第三方app会有一些接口有权限限制
-*/
 public class Util {
 
     /**
@@ -147,6 +149,83 @@ public class Util {
     }
 
 
+    /**
+     * 获得app 的sha1值 *
+     *
+     * @param context
+     * @return
+     */
+    public static String getAppSignSha1(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(), PackageManager.GET_SIGNATURES);
+            // X509证书，X.509是一种非常通用的证书格式
+            Signature[] signs = packageInfo.signatures;
+            Signature sign = signs[0];
+            CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate) certFactory.generateCertificate(new ByteArrayInputStream(sign.toByteArray())); // md5
+            MessageDigest md = MessageDigest.getInstance("SHA1");
+            // 获得公钥
+            byte[] b = md.digest(cert.getEncoded());
+            return byte2HexFormatted(b);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 获得app 的sha1值 *
+     *
+     * @param context
+     * @return
+     */
+    public static String getAppSignMd5(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            //请注意需要PackageManager.GET_SIGNATURES 这个flag
+            PackageInfo packageInfo = packageManager.getPackageInfo(context.getPackageName(),
+                    PackageManager.GET_SIGNATURES);
+            Signature[] signs = packageInfo.signatures;
+            Signature sign = signs[0];
+            // X509证书，X.509是一种非常通用的证书格式
+            CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+            X509Certificate cert = (X509Certificate) certFactory
+                    .generateCertificate(new ByteArrayInputStream(sign.toByteArray()));
+            // md5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 获得公钥
+            byte[] b = md.digest(cert.getEncoded());
+            //key即为应用签名
+            String key = byte2HexFormatted(b).replace(":", "");
+            return key;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 将获取到得编码进行16进制转换
+     *
+     * @param arr
+     * @return
+     */
+    private static String byte2HexFormatted(byte[] arr) {
+        StringBuilder str = new StringBuilder(arr.length * 2);
+        for (int i = 0; i < arr.length; i++) {
+            String h = Integer.toHexString(arr[i]);
+            int l = h.length();
+            if (l == 1)
+                h = "0" + h;
+            if (l > 2)
+                h = h.substring(l - 2, l);
+            str.append(h.toUpperCase());
+            if (i < (arr.length - 1))
+                str.append(':');
+        }
+        return str.toString();
+    }
 
 
 }
